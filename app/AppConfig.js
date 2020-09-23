ddf992e15c578ffac977cf021cc17409b2328bc0
cam2332 <kam2332@gmail.com>
2020-09-23 13:35:31 +0200

    add AppConfig.js

---
+++ app/AppConfig.js
@@ -0,0 +1,13 @@
+let DarkMode = false;
+const PrimaryColor = '#0788D9';
+const SecondaryColor = DarkMode ? '#151515' : '#FAFAFA';
+const SecondaryNegativeColor = DarkMode ? '#FAFAFA' : '#151515';
+const SecondaryHalfColor = '#7a7a7a';
+
+export {
+  DarkMode,
+  PrimaryColor,
+  SecondaryColor,
+  SecondaryNegativeColor,
+  SecondaryHalfColor,
+};
