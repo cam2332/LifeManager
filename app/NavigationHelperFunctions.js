d64bf4906432930282ee7b40f40305d826d216b8
cam2332 <kam2332@gmail.com>
2020-09-23 13:44:47 +0200

    add Navigation loginStack to NavigationHelperFunctions.js

--- app/NavigationHelperFunctions.js
+++ app/NavigationHelperFunctions.js
@@ -1,2 +1,20 @@
 import {DarkMode, PrimaryColor, SecondaryColor} from './AppConfig';
 import {Navigation} from 'react-native-navigation';
+
+const loginStack = {
+  root: {
+    component: {
+      id: 'LoginScreen',
+      name: 'LoginScreen',
+      options: {
+        topBar: {
+          visible: false,
+        },
+        statusBar: {
+          backgroundColor: SecondaryColor,
+          style: DarkMode ? 'light' : 'dark',
+        },
+      },
+    },
+  },
+};
