084ca9fd058b959a1ae80ae1b77ec618675a9b3c
cam2332 <kam2332@gmail.com>
2020-09-29 19:51:17 +0200

    add RandomString function to HelperFunctions.js

--- app/HelperFunctions.js
+++ app/HelperFunctions.js
@@ -1,1 +1,17 @@
 
+export function RandomString(length) {
+  const chars =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const timestamp = (new Date().getTime() / 1000 || 0)
+    .toString(16)
+    .replace('.', '');
+  let result = timestamp;
+  if (result.length > length) {
+    result = result.substring(0, length);
+  } else if (result.length < length) {
+    for (let i = length - result.length; i > 0; --i) {
+      result += chars[Math.round(Math.random() * (chars.length - 1))];
+    }
+  }
+  return result;
+}
