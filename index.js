f6658e6e7b3818fa6a8fb6e405d90ffa176ded5f
cam2332 <kam2332@gmail.com>
2020-09-29 23:34:12 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 import {Navigation} from 'react-native-navigation';
-import {SetLoginRoot} from './app/NavigationHelperFunctions';
+import * as NavigationHelperFunctions from './app/NavigationHelperFunctions';
 import {SecondaryColor} from './app/AppConfig';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 import LoginScreen from './app/screens/LoginScreen';
+import RegisterScreen from './app/screens/RegisterScreen';
+import LeftSideMenu from './app/screens/LeftSideMenu';
+import NoteMainScreen from './app/screens/NoteMainScreen';
+import NoteEditScreen from './app/screens/NoteEditScreen';
 
 Navigation.registerComponent('LoginScreen', () =>
   gestureHandlerRootHOC(LoginScreen),
 );
 
+Navigation.registerComponent('RegisterScreen', () =>
+  gestureHandlerRootHOC(RegisterScreen),
+);
+
+Navigation.registerComponent('LeftSideMenu', () =>
+  gestureHandlerRootHOC(LeftSideMenu),
+);
+
+Navigation.registerComponent('NoteMainScreen', () =>
+  gestureHandlerRootHOC(NoteMainScreen),
+);
+
+Navigation.registerComponent('NoteEditScreen', () =>
+  gestureHandlerRootHOC(NoteEditScreen),
+);
+
+Navigation.setDefaultOptions({
+  layout: {
+    backgroundColor: SecondaryColor,
+  },
+});
+
 Navigation.events().registerAppLaunchedListener(() => {
-  SetLoginRoot();
+  NavigationHelperFunctions.SetNoteRoot();
 });
