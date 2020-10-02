b38670693d477a207fa986b7855aca130f436902
cam2332 <kam2332@gmail.com>
2020-10-03 00:33:17 +0200

    Create SnackBar.js

---
+++ app/components/SnackBar.js
@@ -0,0 +1,97 @@
+import React, {useEffect, useRef} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  DarkMode,
+  SecondaryNegativeColor,
+  PrimaryColor,
+  SecondaryThreeFourthColor,
+} from '../AppConfig';
+
+const SnackBar = (props) => {
+  const OnHideCallback = props.OnHide;
+  let timer = useRef();
+
+  // component did mount
+  useEffect(() => {
+    props.visible &&
+      (timer.current = setTimeout(() => {
+        OnHideCallback();
+      }, props.data.duration || 2000));
+  }, [OnHideCallback, props.data.duration, props.visible]);
+
+  // component will unmount
+  useEffect(() => {
+    return () => {
+      clearTimeout(timer.current);
+    };
+  });
+
+  return props.visible ? (
+    <View style={styles.content}>
+      <View style={styles.descriptionView}>
+        <Text
+          style={[
+            styles.description,
+            !props.buttonVisible ? styles.flex89 : styles.flex100,
+          ]}>
+          {props.description || 'placeholder'}
+        </Text>
+      </View>
+      {!props.buttonVisible && (
+        <TouchableOpacity
+          onPress={() => props.OnPressButton()}
+          style={styles.button}>
+          <Text style={styles.buttonText}>
+            {props.buttonText || 'placeholder'}
+          </Text>
+        </TouchableOpacity>
+      )}
+    </View>
+  ) : (
+    <View />
+  );
+};
+
+const styles = StyleSheet.create({
+  descriptionView: {flex: 1},
+  content: {
+    flex: 1,
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 24,
+    left: 17,
+    right: 17,
+    backgroundColor: DarkMode
+      ? SecondaryThreeFourthColor
+      : SecondaryThreeFourthColor,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 7,
+  },
+  flex89: {
+    flex: 0.89,
+  },
+  flex100: {
+    flex: 1,
+  },
+  description: {
+    flexWrap: 'wrap',
+    color: SecondaryNegativeColor,
+    fontSize: 14,
+    marginVertical: 4,
+  },
+  button: {
+    paddingLeft: 10,
+    marginVertical: 4,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '900',
+    color: PrimaryColor,
+  },
+});
+
+export default SnackBar;
