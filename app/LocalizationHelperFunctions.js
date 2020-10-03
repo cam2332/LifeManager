f267fb734667c25e3bc3a2b828504671427d9966
cam2332 <kam2332@gmail.com>
2020-10-03 11:09:19 +0200

    add time function to LocalizationHelperFunctions.js

--- app/LocalizationHelperFunctions.js
+++ app/LocalizationHelperFunctions.js
@@ -1,3 +1,11 @@
+export function time(date) {
+  return (
+    (date.getHours() > 9 ? date.getHours() : '0' + date.getHours()) +
+    ':' +
+    (date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes())
+  );
+}
+
 export function datePL(date, {showYear = true, showWeekDay = true} = {}) {
   return (
     (showWeekDay ? daysOfWeekPL[date.getDay()] + ', ' : '') +
