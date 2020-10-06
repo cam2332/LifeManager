1537f37995d926aa6d83be4e918cd8c4b6709219
cam2332 <kam2332@gmail.com>
2020-10-06 14:46:27 +0200

    add ScreenHeader to TodoMainScreen

--- app/screens/TodoMainScreen.js
+++ app/screens/TodoMainScreen.js
@@ -1,8 +1,49 @@
 import React from 'react';
-import {View} from 'react-native';
+import {
+  View,
+  StyleSheet,
+} from 'react-native';
+import {
+  DarkMode,
+  PrimaryColor,
+  SecondaryColor,
+  SecondaryNegativeColor,
+  SecondaryThreeFourthColor,
+} from '../AppConfig';
+import ScreenHeader from '../components/ScreenHeader';
 
-const TodoMainScreen = () => {
-  return <View />;
+const TodoMainScreen = (props) => {
+
+  const UpdateListFromSearch = (text) => {
+  };
+
+  const OnChangeSearchText = (newValue) => {
+    UpdateListFromSearch(newValue);
+  };
+
+  return (
+    <View style={styles.mainContainer}>
+      <ScreenHeader
+        title={'Lista zadań'}
+        textColor={DarkMode ? SecondaryNegativeColor : PrimaryColor}
+        iconsColor={DarkMode ? SecondaryNegativeColor : PrimaryColor}
+        backgroundColor={DarkMode ? SecondaryThreeFourthColor : SecondaryColor}
+        borderColor={DarkMode ? SecondaryThreeFourthColor : PrimaryColor}
+        sideMenuButtonVisible={true}
+        searchButtonVisible={true}
+        searchBarTextPlaceholder={'Wpisz nazwę listy'}
+        OnChangeSearchText={(text) => OnChangeSearchText(text)}
+        OnEndTypingSearch={(text) => UpdateListFromSearch(text)}
+        OnCancelTypingSearch={UpdateTodoList}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+});
 
 export default TodoMainScreen;
