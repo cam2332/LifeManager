4a9a61e0c7049f6caadad001c3556e2f35c94bae
cam2332 <kam2332@gmail.com>
2020-11-16 15:46:15 +0100

    Add LoginUser to UserApi.js

--- app/services/UserApi.js
+++ app/services/UserApi.js
@@ -1,4 +1,6 @@
 import {serverAddress} from '../AppConfig';
+import PouchDB from 'pouchdb-react-native';
+const db = new PouchDB('mydb');
 
 export const RegisterUser = async (login, email, password) => {
   return new Promise((resolve, reject) => {
@@ -16,13 +18,51 @@ export const RegisterUser = async (login, email, password) => {
       })
       .then(([statusCode, data]) => {
         if (statusCode === 201) {
-          resolve([statusCode, data]);
+          resolve(data);
         } else if (statusCode === 409) {
           reject(data);
         }
       })
       .catch((error) => {
         console.error(error);
+        return error;
+      });
+  });
+};
+
+export const LoginUser = async (login, password) => {
+  return new Promise((resolve, reject) => {
+    return fetch(serverAddress + 'user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        login,
+        password,
+        deviceName: 'DeviceName',
+      }),
+    })
+      .then((response) => {
+        const statusCode = response.status;
+        const data = response.json();
+        return Promise.all([statusCode, data]);
+      })
+      .then(([statusCode, data]) => {
+        if (statusCode === 200) {
+          db.post({token: data.token}, (err, res) => {
+            if (!err) {
+              resolve();
+            } else {
+              reject();
+            }
+          });
+        } else if (statusCode === 401) {
+          reject(data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
         return error;
       });
   });
