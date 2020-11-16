01bba259abf33b967d9bd4cd3ab992a615036041
cam2332 <kam2332@gmail.com>
2020-11-16 15:47:19 +0100

    Update RegisterScreen.js

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {
   StyleSheet,
   View,
@@ -6,38 +6,91 @@ import {
   Image,
   TouchableOpacity,
   TextInput,
+  KeyboardAvoidingView,
 } from 'react-native';
 import {
   darkMode,
-  PRIMARY_COLOR,
-  SECONDARY_COLOR,
-  SECONDARY_NEGATIVE_COLOR,
+  primaryColor,
+  secondaryColor,
+  secondaryNegativeColor,
   SECONDARY_HALF_COLOR,
+  secondaryThreeFourthColor,
 } from '../AppConfig';
-import {SetLoginRoot} from '../NavigationHelperFunctions';
 import LogoIcon from '../../resources/LogoIcon.png';
-
-const OnPressRegister = (a, b) => {
-  console.log(a, b);
-};
+import * as UserApi from '../services/UserApi';
+import SnackBar from '../components/SnackBar';
+import CustomTextInput from '../components/CustomTextInput';
+import * as NavigationHelperFunctions from '../NavigationHelperFunctions';
 
 const OnPressGoToLoginScreen = () => {
-  SetLoginRoot();
+  NavigationHelperFunctions.SetLoginRoot();
 };
 
-const RegisterScreen = () => {
+const RegisterScreen = (props) => {
   const [loginText, setLoginText] = useState('');
   const [emailText, setEmailText] = useState('');
   const [passwordText, setPasswordText] = useState('');
   const [confirmPasswordText, setConfirmPasswordText] = useState('');
+  const [isFormValid, setIsFormValid] = useState(false);
+  const loginInputRef = useRef();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const confirmPasswordInputRef = useRef();
+  const snackBarRef = useRef();
 
-  const AllFieldsFilled = () => {
-    return (
-      loginText !== '' &&
-      emailText !== '' &&
-      passwordText !== '' &&
-      confirmPasswordText !== ''
+  useEffect(() => {
+    NavigationHelperFunctions.UpdateStatusBarColor(
+      props.componentId,
+      secondaryColor,
     );
+    NavigationHelperFunctions.SetCurrentScreenId(props.componentId);
+  });
+
+  useEffect(() => {
+    setIsFormValid(
+      loginText !== '' &&
+        emailText !== '' &&
+        passwordText !== '' &&
+        confirmPasswordText !== '' &&
+        passwordText === confirmPasswordText &&
+        !loginInputRef.current.hasError() &&
+        !emailInputRef.current.hasError() &&
+        !passwordInputRef.current.hasError() &&
+        !confirmPasswordInputRef.current.hasError(),
+    );
+  }, [loginText, emailText, passwordText, confirmPasswordText]);
+
+  const OnPressRegister = () => {
+    UserApi.RegisterUser(loginText, emailText, passwordText)
+      .then((data) => {
+        // Auto login after successful registration
+        UserApi.LoginUser(loginText, passwordText)
+          .then(() => {
+            NavigationHelperFunctions.SetNoteRoot();
+          })
+          .catch((error) => {
+            if (error) {
+              snackBarRef.current.ShowSnackBar(error?.message, 2000, false);
+            }
+          });
+      })
+      .catch((error) => {
+        snackBarRef.current.ShowSnackBar(error.message, 2000, '', false);
+        if (error.additionalInfo === 'login') {
+          loginInputRef.current.setErrorValue(
+            true,
+            'Nazwa użytkownika w użyciu',
+          );
+        } else if (error.additionalInfo === 'email') {
+          emailInputRef.current.setErrorValue(true, 'Adres email w użyciu');
+        } else if (error.additionalInfo === '') {
+          loginInputRef.current.setErrorValue(
+            true,
+            'Nazwa użytkownika w użyciu',
+          );
+          emailInputRef.current.setErrorValue(true, 'Adres email w użyciu');
+        }
+      });
   };
 
   return (
@@ -49,59 +102,167 @@ const RegisterScreen = () => {
         <Text style={styles.registerText}>Rejestracja</Text>
       </View>
       <View style={styles.sectionBody}>
-        <TextInput
-          style={styles.textInput}
-          color={SECONDARY_NEGATIVE_COLOR}
-          underlineColorAndroid={'#c4c4c4'}
+        <CustomTextInput
+          ref={loginInputRef}
+          viewStyle={styles.textInput}
+          color={secondaryNegativeColor}
+          underlineColorAndroid={secondaryThreeFourthColor}
           placeholder={'Nazwa użytkownika'}
           placeholderTextColor={SECONDARY_HALF_COLOR}
-          onChangeText={(text) => setLoginText(text)}
+          inputText={loginText}
+          onChangeText={(text) => {
+            setLoginText(text);
+            loginText.length === 0
+              ? loginInputRef.current.setErrorValue(true, 'Wprowadź nazwę')
+              : loginInputRef.current.setErrorValue(false, '');
+          }}
+          onKeyPress={({nativeEvent}) => {
+            if (nativeEvent.key === 'Backspace') {
+              loginText.length === 0
+                ? loginInputRef.current.setErrorValue(true, 'Wprowadź nazwę')
+                : loginInputRef.current.setErrorValue(false, '');
+            }
+          }}
         />
-        <TextInput
-          style={styles.textInput}
-          color={SECONDARY_NEGATIVE_COLOR}
-          underlineColorAndroid={'#c4c4c4'}
+        <CustomTextInput
+          ref={emailInputRef}
+          viewStyle={styles.textInput}
+          color={secondaryNegativeColor}
+          underlineColorAndroid={secondaryThreeFourthColor}
           placeholder={'Email'}
           placeholderTextColor={SECONDARY_HALF_COLOR}
-          onChangeText={(text) => setEmailText(text)}
+          inputText={emailText}
+          onChangeText={(text) => {
+            setEmailText(text);
+            const regex = new RegExp(/\S+@\S+\.\S+/);
+            text.length === 0
+              ? emailInputRef.current.setErrorValue(
+                  true,
+                  'Wprowadź prawidłowy email',
+                )
+              : emailInputRef.current.setErrorValue(false, '');
+            !regex.test(text)
+              ? emailInputRef.current.setErrorValue(
+                  true,
+                  'Wprowadź prawidłowy email',
+                )
+              : emailInputRef.current.setErrorValue(false, '');
+          }}
+          onKeyPress={({nativeEvent}) => {
+            if (nativeEvent.key === 'Backspace') {
+              const regex = new RegExp(/\S+@\S+\.\S+/);
+              emailText.length === 0
+                ? emailInputRef.current.setErrorValue(
+                    true,
+                    'Wprowadź prawidłowy email',
+                  )
+                : emailInputRef.current.setErrorValue(false, '');
+              !regex.test(emailText)
+                ? emailInputRef.current.setErrorValue(
+                    true,
+                    'Wprowadź prawidłowy email',
+                  )
+                : emailInputRef.current.setErrorValue(false, '');
+            }
+          }}
         />
-        <TextInput
-          style={styles.inputText}
-          color={SECONDARY_NEGATIVE_COLOR}
-          underlineColorAndroid={'#c4c4c4'}
+        <CustomTextInput
+          ref={passwordInputRef}
+          viewStyle={styles.inputText}
+          color={secondaryNegativeColor}
+          underlineColorAndroid={secondaryThreeFourthColor}
           placeholder={'Hasło'}
           placeholderTextColor={SECONDARY_HALF_COLOR}
-          onChangeText={(text) => setPasswordText(text)}
+          inputText={passwordText}
+          onChangeText={(text) => {
+            setPasswordText(text);
+            text.length < 6
+              ? passwordInputRef.current.setErrorValue(
+                  true,
+                  'Długość hasła powinna wynosić minimum 6 znaków',
+                )
+              : passwordInputRef.current.setErrorValue(false, '');
+            confirmPasswordText !== text
+              ? confirmPasswordInputRef.current.setErrorValue(
+                  true,
+                  'Hasła muszą być identyczne',
+                )
+              : confirmPasswordInputRef.current.setErrorValue(false, '');
+          }}
+          onKeyPress={({nativeEvent}) => {
+            if (nativeEvent.key === 'Backspace') {
+              passwordText.length === 0
+                ? passwordInputRef.current.setErrorValue(true, 'Wprowadź hasło')
+                : passwordInputRef.current.setErrorValue(false, '');
+              passwordText.length < 6
+                ? passwordInputRef.current.setErrorValue(
+                    true,
+                    'Długość hasła powinna wynosić minimum 6 znaków',
+                  )
+                : passwordInputRef.current.setErrorValue(false, '');
+              confirmPasswordText !== passwordText
+                ? confirmPasswordInputRef.current.setErrorValue(
+                    true,
+                    'Hasła muszą być identyczne',
+                  )
+                : confirmPasswordInputRef.current.setErrorValue(false, '');
+            }
+          }}
           secureTextEntry={true}
         />
-        <TextInput
-          style={styles.inputText}
-          color={SECONDARY_NEGATIVE_COLOR}
-          underlineColorAndroid={'#c4c4c4'}
+        <CustomTextInput
+          ref={confirmPasswordInputRef}
+          viewStyle={styles.inputText}
+          color={secondaryNegativeColor}
+          underlineColorAndroid={secondaryThreeFourthColor}
           placeholder={'Powtórz hasło'}
           placeholderTextColor={SECONDARY_HALF_COLOR}
-          onChangeText={(text) => setConfirmPasswordText(text)}
+          inputText={confirmPasswordText}
+          onChangeText={(text) => {
+            setConfirmPasswordText(text);
+            passwordText !== text
+              ? confirmPasswordInputRef.current.setErrorValue(
+                  true,
+                  'Hasła muszą być identyczne',
+                )
+              : confirmPasswordInputRef.current.setErrorValue(false, '');
+          }}
+          onKeyPress={({nativeEvent}) => {
+            if (nativeEvent.key === 'Backspace') {
+              confirmPasswordText.length === 0
+                ? confirmPasswordInputRef.current.setErrorValue(
+                    true,
+                    'Wprowadź hasło',
+                  )
+                : confirmPasswordInputRef.current.setErrorValue(false, '');
+            }
+          }}
           secureTextEntry={true}
         />
         <TouchableOpacity
           style={[
             styles.registerButton,
             {
-              backgroundColor: AllFieldsFilled
-                ? PRIMARY_COLOR
-                : SECONDARY_COLOR,
+              backgroundColor: isFormValid
+                ? primaryColor
+                : darkMode
+                ? secondaryThreeFourthColor
+                : SECONDARY_HALF_COLOR,
             },
           ]}
+          disabled={!isFormValid}
           onPress={OnPressRegister}>
           <Text
             style={[
               styles.registerButtonText,
               {
-                color: AllFieldsFilled
+                color: isFormValid
                   ? darkMode
-                    ? SECONDARY_NEGATIVE_COLOR
-                    : SECONDARY_COLOR
-                  : PRIMARY_COLOR,
+                    ? secondaryNegativeColor
+                    : secondaryColor
+                  : darkMode
+                  ? SECONDARY_HALF_COLOR
+                  : secondaryThreeFourthColor,
               },
             ]}>
             Zarejestruj
@@ -116,6 +277,7 @@ const RegisterScreen = () => {
           </View>
         </TouchableOpacity>
       </View>
+      <SnackBar ref={snackBarRef} />
     </View>
   );
 };
@@ -123,24 +285,24 @@ const RegisterScreen = () => {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    backgroundColor: SECONDARY_COLOR,
+    backgroundColor: secondaryColor,
   },
   sectionLogo: {
     alignItems: 'center',
     flex: 0.4,
-    backgroundColor: SECONDARY_COLOR,
+    backgroundColor: secondaryColor,
     justifyContent: 'center',
   },
   sectionTitle: {
     flex: 0.2,
-    backgroundColor: SECONDARY_COLOR,
+    backgroundColor: secondaryColor,
   },
   image: {
     resizeMode: 'contain',
     flex: 0.6,
   },
   registerText: {
-    color: PRIMARY_COLOR,
+    color: primaryColor,
     fontSize: 44,
     alignSelf: 'center',
     marginVertical: 2,
@@ -162,12 +324,12 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
   },
   loginHintText: {
-    color: PRIMARY_COLOR,
+    color: primaryColor,
     fontSize: 18,
     alignSelf: 'center',
   },
   loginHintBoldText: {
-    color: PRIMARY_COLOR,
+    color: primaryColor,
     fontSize: 18,
     alignSelf: 'center',
     fontWeight: 'bold',
@@ -179,8 +341,6 @@ const styles = StyleSheet.create({
     width: '65%',
     paddingHorizontal: 24,
     paddingVertical: 6,
-    borderColor: PRIMARY_COLOR,
-    borderWidth: 1,
   },
   registerButtonText: {
     fontSize: 18,
@@ -190,7 +350,7 @@ const styles = StyleSheet.create({
   sectionBody: {
     flex: 0.65,
     justifyContent: 'space-around',
-    backgroundColor: SECONDARY_COLOR,
+    backgroundColor: secondaryColor,
     paddingBottom: 40,
   },
   inputText: {
