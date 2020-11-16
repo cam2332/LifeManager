cb2d63ff58125553968566ff53d290d136356c18
cam2332 <kam2332@gmail.com>
2020-11-16 13:28:04 +0100

    Add RegisterUser to UserApi.js

--- app/services/UserApi.js
+++ app/services/UserApi.js
@@ -1,1 +1,29 @@
 import {serverAddress} from '../AppConfig';
+
+export const RegisterUser = async (login, email, password) => {
+  return new Promise((resolve, reject) => {
+    return fetch(serverAddress + 'user/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({login, email, password}),
+    })
+      .then((response) => {
+        const statusCode = response.status;
+        const data = response.json();
+        return Promise.all([statusCode, data]);
+      })
+      .then(([statusCode, data]) => {
+        if (statusCode === 201) {
+          resolve([statusCode, data]);
+        } else if (statusCode === 409) {
+          reject(data);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        return error;
+      });
+  });
+};
