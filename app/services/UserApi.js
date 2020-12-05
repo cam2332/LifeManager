bbc99cc71c0b3d0a8e3fe1a3193af9f801b02b9a
cam2332 <kam2332@gmail.com>
2020-12-05 20:44:47 +0100

    Add logout function to UserApi

--- app/services/UserApi.js
+++ app/services/UserApi.js
@@ -1,5 +1,6 @@
 import {serverAddress} from '../AppConfig';
 import * as SettingsApi from './SettingsApi';
+import * as NavigationHelperFunctions from '../NavigationHelperFunctions';
 
 export const RegisterUser = async (login, email, password) => {
   return new Promise((resolve, reject) => {
@@ -20,6 +21,8 @@ export const RegisterUser = async (login, email, password) => {
           resolve(data);
         } else if (statusCode === 409) {
           reject(data);
+        } else {
+          reject();
         }
       })
       .catch((error) => {
@@ -58,6 +61,8 @@ export const LoginUser = async (login, password) => {
           }
         } else if (statusCode === 401) {
           reject(data);
+        } else {
+          reject();
         }
       })
       .catch((error) => {
@@ -65,4 +70,42 @@ export const LoginUser = async (login, password) => {
         return error;
       });
   });
+};
+
+export const LogoutUser = async () => {
+  const token = await SettingsApi.GetAccessToken();
+  return new Promise((resolve, reject) => {
+    return fetch(serverAddress + 'user/logout', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    })
+      .then((response) => {
+        const statusCode = response.status;
+        const data = response.json();
+        return Promise.all([statusCode, data]);
+      })
+      .then(async ([statusCode, data]) => {
+        if (statusCode === 200 || statusCode === 404) {
+          resolve();
+        } else if (statusCode === 400 || statusCode === 401) {
+          await ClearUserTokenAndGoToLoginScreen();
+          reject(data);
+        } else {
+          reject();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  });
+};
+
+export const ClearUserTokenAndGoToLoginScreen = async () => {
+  await SettingsApi.DeleteAccessToken();
+  await SettingsApi.DeleteUserData();
+  NavigationHelperFunctions.SetLoginRoot();
 };
